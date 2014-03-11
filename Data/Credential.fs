f744b2d8d9c81ffca319d88ea9aa2f4eac5c3a43
j-alexander <jleaver@alumni.uwo.ca>
2014-03-10 20:23:14 -0400

    Excludes password from a serialized user.

---
+++ Data/Credential.fs
@@ -0,0 +1,8 @@
+﻿namespace SkyVue.Data
+
+type Credential = Schema.ServiceTypes.Credential
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Credential =
+    let id (x : Credential) = x.UserId
+    let password (x : Credential) = x.Password
